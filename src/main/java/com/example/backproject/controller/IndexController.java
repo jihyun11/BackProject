f6fd05b907a038c3ce73ce83276803525ce4532d
지현 <372491@naver.com>
2022-11-02 22:53:57 +0900

    placeholder 지우기

--- src/main/java/com/example/backproject/controller/IndexController.java
+++ src/main/java/com/example/backproject/controller/IndexController.java
@@ -1,8 +1,18 @@
 package com.example.backproject.controller;
 
+import com.example.backproject.dto.MemberDto;
+import com.example.backproject.service.SignService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.SessionAttribute;
 import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import java.security.Principal;
 
 @Controller
 public class IndexController {
@@ -16,6 +26,4 @@ public class IndexController {
         ModelAndView mav = new ModelAndView("myinfo");
         return mav;
     }
-
-
 }
