a35e4757caea61b0b8143095438125a7b33ebb80
지현 <372491@naver.com>
2022-11-03 18:15:24 +0900

    myinfo 컨트롤러

--- src/main/java/com/example/backproject/controller/MyinfoController.java
+++ src/main/java/com/example/backproject/controller/MyinfoController.java
@@ -2,11 +2,10 @@ package com.example.backproject.controller;
 
 import com.example.backproject.dto.MemberDto;
 import com.example.backproject.service.SignService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.SessionAttribute;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -15,11 +14,24 @@ import javax.servlet.http.HttpSession;
 import java.security.Principal;
 
 @Controller
-public class IndexController {
-    @GetMapping("/")
-    public ModelAndView index() {
-        ModelAndView mav = new ModelAndView("index");
+@RequiredArgsConstructor
+public class MyinfoController {
+
+    @GetMapping("/myinfo")
+    public ModelAndView myinfo() {
+        ModelAndView mav = new ModelAndView("myinfo");
         return mav;
     }
+
+    @GetMapping("/myinfo/username")
+    public String myinfoUsername(@ModelAttribute MemberDto md, HttpServletRequest req) {
+
+        System.out.println(md.getUsername() + md.getPassword());
+        String memberDto = md.getUsername();
+        System.out.println(memberDto);
+        HttpSession session = req.getSession();
+        return md.getUsername();
+    }
+
 
 }
